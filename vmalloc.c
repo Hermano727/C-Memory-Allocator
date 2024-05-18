00de901aa39ffa955662b81aed9f4a4b5fe7577a
Herman Hundsberger <hhundsberger@ieng6-202.ucsd.edu>
2024-05-17 17:37:36 -0700

    did size block calcuations and heap traversal for best fit policy.

--- vmalloc.c
+++ vmalloc.c
@@ -3,6 +3,38 @@
 
 void *vmalloc(size_t size)
 {
-    // TODO
-    return NULL;
+    if (size < 0 ) {
+        return NULL; //no allocation made
+    }  
+
+    // Find total size needed for allocation
+    size_t total_size = size + sizeof(struct block_header);
+    total_size = ROUND_UP(total_size, BLKALIGN); //Round up to nearest 16 
+    
+    struct block_header *current = heapstart;
+    struct block_header *best_fit = NULL;
+
+    //While not at the end of the block, traverse through the block
+    while (current->size_status != VM_ENDMARK) {
+        size_t curr_block_sz = BLKSZ(current);
+        
+        // Use bit operators to check if space is available at current block.
+        if (!(current->size_status & VM_BUSY) && curr_block_sz >= total_size) {
+            if (best_fit == NULL || curr_block_sz < BLKSZ(best_fit)) {
+                    best_fit = current; // Set the pointer to the best found block
+            }
+        }
+
+        //Traverse to next block
+        curr_block_sz = (struct block_header *)((char *) current + current_block_size);
+
+    }
+
+    // If there is no space, return null
+    if (best_fit == NULL) {
+        return NULL;
+    }
+
+    //Return the pointer to the payload
+    return (void *)((char *)best_fit + sizeof(struct block_header));
 }
