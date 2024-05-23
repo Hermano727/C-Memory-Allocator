9a198f0a79d83bfd2bfb3a99602f66ebf5ae7082
Herman Hundsberger <hhundsberger@ieng6-202.ucsd.edu>
2024-05-23 00:16:10 -0700

    oops it was a syntax error. IM FREEEE get it haha

--- vmfree.c
+++ vmfree.c
@@ -24,7 +24,7 @@ void vmfree(void *ptr)
    // Unset status bit to 0
    curr->size_status &= ~VM_BUSY;
     
-   struct block_header *current_footer = (struct block_footer *)((char *)curr + BLKSZ(curr) - sizeof(struct block_footer));
+   struct block_footer *current_footer = (struct block_footer *)((char *)curr + BLKSZ(curr) - sizeof(struct block_footer));
    current_footer->size = BLKSZ(curr);
 
    struct block_header *next_block = (struct block_header *)((char *)curr + BLKSZ(curr));
@@ -36,14 +36,14 @@ void vmfree(void *ptr)
                 curr->size_status += BLKSZ(next_block);
                 struct block_footer *next_footer = (struct block_footer *)((char *)next_block + BLKSZ(next_block) - sizeof(struct block_footer));
                 current_footer = next_footer;
-                current_footer->size = BLKSZ(current);
+                current_footer->size = BLKSZ(curr);
         }
    }
 
-   if (!(current->size_status & VM_PREVBUSY)) {
-           struct block_footer *prev_footer = (struct block_footer *)((char *)current - sizeof(struct block_footer));
-           struct block_header *prev_block = (struct block_header *)((char *)current - prev_footer->size);
-           prev_block->size_status += BLKSZ(current);
+   if (!(curr->size_status & VM_PREVBUSY)) {
+           struct block_footer *prev_footer = (struct block_footer *)((char *)curr - sizeof(struct block_footer));
+           struct block_header *prev_block = (struct block_header *)((char *)curr - prev_footer->size);
+           prev_block->size_status += BLKSZ(curr);
            current_footer->size = BLKSZ(prev_block);
    }
 
