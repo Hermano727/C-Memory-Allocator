0211c752a6ab4a83c35fcba723298fc2e8a28c88
Herman Hundsberger <hhundsberger@ieng6-202.ucsd.edu>
2024-05-22 21:55:31 -0700

    attempting coalescing of sorts

--- vmfree.c
+++ vmfree.c
@@ -14,14 +14,31 @@ void vmfree(void *ptr)
       return;
    }
    
-   struct block_header *current = (struct block_header *)((char *)ptr - sizeof(struct block_header));
+   struct block_header *curr = (struct block_header *)((char *)ptr - sizeof(struct block_header));
 
    // If block header indicates the block is already free, do nothing
-   if (!(current->size_status & VM_BUSY)) {
+   if (!(curr->size_status & VM_BUSY)) {
            return;
    }
 
    // Unset status bit to 0
-   current->size_status &= ~VM_BUSY;
-   
+   curr->size_status &= ~VM_BUSY;
+    
+   struct block_header *curr_footer = (struct block_footer *)((char *)curr + BLKSZ(current) - sizeof(struct block_footer));
+   curr_footer->size = BLKSZ(curr);
+
+   struct block_header *next_block = (struct block_header *)((char *)curr + BLKSZ(curr));
+   if (next_block->size_status != VM_ENDMARK) {
+           next_block->size_status &= ~VM_PREVBUSY;
+
+        // Coalesce with next block if it is also free
+        if (!(next_block->size_status & VM_BUSY)) {
+                curr->size_status += BLKSZ(next_block);
+                struct block_footer *next_footer = (struct block_footer *)((char *)next_block + BLKSZ(next_block) - sizeof(struct block_footer));
+                curr_footer = next_footer;
+                curr_footer->size = BLKSZ(current);
+        }
+   }
+
+    
 }
